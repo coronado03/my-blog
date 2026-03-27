22402cca1cf9f9a017c787e66949414c7922cb16
coronado03 <sebascoronado03@hotmail.com>
2026-03-27 19:35:23 +0100

    fix(books): Fixed books tightly coupled code for scalibility, separated stuff in different files etc.

--- src/components/books/books.tsx
+++ src/components/books/books.tsx
@@ -1,49 +1,7 @@
-type Book = {
-  title: string;
-  author: string;
-  progress: number;
-  status: 'Reading' | 'Done' | 'Queue';
-  year: number;
-}
+import { Book } from "@/types/book";
+import { books } from "@/content/data/books"
 
 export default function Books() {
-  const books: Book[] = [
-    {
-      title: "C: A Modern Approach",
-      author: "K.N. King",
-      progress: 72,
-      status: "Reading",
-      year: 2008
-    },
-    {
-      title: "Computer Networking: A Top-Down Approach",
-      author: "James F. Kurose & Keith W. Ross",
-      progress: 19,
-      status: "Reading",
-      year: 2021
-    },
-    {
-      title: "Effective TypeScript",
-      author: "Dan Vanderkam",
-      progress: 2,
-      status: "Reading",
-      year: 2019
-    },
-    {
-      title: "CS:APP - Computer Systems",
-      author: "Bryant & O'Hallaron",
-      progress: 0,
-      status: "Queue",
-      year: 2015
-    },
-    {
-      title: "SICP",
-      author: "Abelson & Sussman",
-      progress: 0,
-      status: "Queue",
-      year: 1996
-    },
-  ];
 
   const getProgressBar = (progress: number) => {
     const filled = Math.round(progress / 5);
